0acf199fef981ca93519749987e8e1161d1261d4
Oana Fatu <oana.fatu@icloud.com>
2019-08-21 10:16:49 +0200

    Updates

---
+++ precache-manifest.af905120e612052a7a2f4681431effa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab624fbd20e100e7670b72add140c1a",
+    "url": "/Booklub/index.html"
+  },
+  {
+    "revision": "5d30a8a391f93b746f98",
+    "url": "/Booklub/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "12a0f771f8785780d1db",
+    "url": "/Booklub/static/css/main.4e868c70.chunk.css"
+  },
+  {
+    "revision": "5d30a8a391f93b746f98",
+    "url": "/Booklub/static/js/2.dfaba530.chunk.js"
+  },
+  {
+    "revision": "12a0f771f8785780d1db",
+    "url": "/Booklub/static/js/main.839d62e1.chunk.js"
+  },
+  {
+    "revision": "9eec5a5855ec75f8ebed",
+    "url": "/Booklub/static/js/runtime~main.2535f56e.js"
+  }
+]);
