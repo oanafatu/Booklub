9ca677cbfd3dc2ff080839e0e6a115c2c7351440
Oana Fatu <oana.fatu@icloud.com>
2019-08-22 11:00:21 +0200

    Updates

--- precache-manifest.d5d197d9570a753155d640e9fd0d72d4.js
+++ precache-manifest.d5d197d9570a753155d640e9fd0d72d4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ff1f0f5e5e00c22df0b51c17f73f847a",
+    "revision": "ffae3abaeec8ec506f2e59488665f425",
     "url": "/Booklub/index.html"
   },
   {
-    "revision": "5d30a8a391f93b746f98",
+    "revision": "906915ec927649ea9d5d",
     "url": "/Booklub/static/css/2.39017165.chunk.css"
   },
   {
-    "revision": "9c721d32f9e9aaa5c7e3",
+    "revision": "61bcbcecb5bf5979adf9",
     "url": "/Booklub/static/css/main.4e868c70.chunk.css"
   },
   {
-    "revision": "5d30a8a391f93b746f98",
-    "url": "/Booklub/static/js/2.dfaba530.chunk.js"
+    "revision": "906915ec927649ea9d5d",
+    "url": "/Booklub/static/js/2.95c14e2b.chunk.js"
   },
   {
-    "revision": "9c721d32f9e9aaa5c7e3",
-    "url": "/Booklub/static/js/main.d9ba0dd9.chunk.js"
+    "revision": "61bcbcecb5bf5979adf9",
+    "url": "/Booklub/static/js/main.fd5933a8.chunk.js"
   },
   {
     "revision": "9eec5a5855ec75f8ebed",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Booklub/precache-manifest.32451eb7cb551ac96ccdd156b5d27145.js"
+  "/Booklub/precache-manifest.d5d197d9570a753155d640e9fd0d72d4.js"
 );
 
 self.addEventListener('message', (event) => {
