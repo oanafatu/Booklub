d75c0ab0626cfde77477d5102976006700870272
Oana Fatu <oana.fatu@icloud.com>
2019-08-27 11:44:18 +0200

    Updates

--- precache-manifest.1bdc9ce9fdf39c72ee56a9d7db0bd588.js
+++ precache-manifest.1bdc9ce9fdf39c72ee56a9d7db0bd588.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7e6564978b177a977b9402d169266988",
+    "revision": "fc860a69af403947721dc7f6f467d8ea",
     "url": "/Booklub/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Booklub/static/css/2.39017165.chunk.css"
   },
   {
-    "revision": "51f5f819708d97f7662d",
+    "revision": "266c642f26d5bdaccc95",
     "url": "/Booklub/static/css/main.4e868c70.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Booklub/static/js/2.95c14e2b.chunk.js"
   },
   {
-    "revision": "51f5f819708d97f7662d",
-    "url": "/Booklub/static/js/main.1167f5da.chunk.js"
+    "revision": "266c642f26d5bdaccc95",
+    "url": "/Booklub/static/js/main.4b045f2f.chunk.js"
   },
   {
     "revision": "9eec5a5855ec75f8ebed",
